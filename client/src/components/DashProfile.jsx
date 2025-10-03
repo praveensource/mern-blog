b074d612687784c19af3c15ef1db3f882c776c91
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-10-04 02:14:11 +0530

    user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,17 +1,104 @@
-import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import { Alert, Button, TextInput } from 'flowbite-react'
+import React, { useEffect, useRef, useState } from 'react'
 import {useSelector} from 'react-redux'
+import axios from 'axios'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 const DashProfile = () => {
-    const {currentUser} = useSelector((state)=> state.user)
+    const { currentUser, error, loading } = useSelector((state) => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+     const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [formData, setFormData] = useState({})
+    const filePickerRef = useRef();
+
+    const handleImageChange = (e) =>{
+      const file = e.target.files[0];
+      if (file) {
+      if (file.size > 2 * 1024 * 1024 || !["image/jpeg", "image/png", "image/jpg"].includes(file.type)) {
+        setImageFileUploadError
+        ("Could not upload image (File must be less than allowed size)")
+      }
+      
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+    useEffect(()=>{
+      if(imageFile){
+        uploadImage();
+      }
+    }, [imageFile]);
+
+    const uploadImage = async()=>{
+      setImageFileUploadError(null)
+      try {
+        const formDataCloud = new FormData();
+      formDataCloud.append("file", imageFile);
+      formDataCloud.append("upload_preset", "react_profile_upload");
+      formDataCloud.append("folder", "users/profiles");
+
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/dujkaa9bu/image/upload", 
+        formDataCloud,
+        {
+          onUploadProgress: (progressEvent) => {
+            const progress = (progressEvent.loaded / progressEvent.total) * 100;
+            setImageFileUploadProgress(progress.toFixed(0));
+          },
+        }
+      );
+
+      const downloadURL = res.data.secure_url;
+      setImageFileUrl(downloadURL);
+      setFormData((formData) => ({ ...formData, profilePicture: downloadURL }));
+      setImageFileUploading(false);
+      setImageFileUploadProgress(null);
+
+      } catch (error) {
+        console.error(error);
+      setImageFileUploadError("Could not upload image (File must be less than allowed size)");
+      setImageFileUploading(false);
+      setImageFileUploadProgress(null);
+      setImageFile(null);
+      setImageFileUrl(null);
+      }
+    }
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
       
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-            <img src={currentUser.profilePicture} alt="user_profile_photo" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
+        
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden />
+
+        <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>filePickerRef.current.click()}>
+          {imageFileUploadProgress && (
+            <CircularProgressbar value={imageFileUploadProgress || 0} text={`${imageFileUploadProgress}%`} strokeWidth={5} styles={{
+              root:{
+                width:'100%',
+                height:'100%',
+                position:'absolute',top:0,left:0,
+              },
+              path:{
+                stroke:`rgba(62,152,99,${imageFileUploadProgress / 100})`
+              }
+            }} />
+          )}
+            <img src={imageFileUrl || currentUser.profilePicture} alt="user_profile_photo" className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'}`} />
         </div>
+
+
+        
+          {imageFileUploadError && (
+            <Alert color='failure'>{imageFileUploadError}</Alert>
+          )}
+        
 
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
         <TextInput type='email' id='email' placeholder='username' defaultValue={currentUser.email} />
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -19,7 +19,7 @@ const DashSidebar = () => {
       <SidebarItems>
         <SidebarItemGroup>
             <Link to={'/dashboard?tab=profile'}>
-            <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+            <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark' as={'div'}>
                 Profile
             </SidebarItem>
             </Link>
