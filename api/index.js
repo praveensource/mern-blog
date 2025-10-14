339dc0c5c05b73b42b083f36584a082dbec5c4c5
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-10-14 15:14:00 +0530

    created post api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js'
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.listen(3000, () =>{
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes)
 
 // adding middleware funcs to manage the errors
 app.use((err, req, res, next)=>{
