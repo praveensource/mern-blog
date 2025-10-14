c75ef9fe5e9626ccccfe2a3273fc2c4f80b9fe75
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-10-14 16:39:40 +0530

    create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,173 @@
-import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+// import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+// import { Editor } from "react-draft-wysiwyg";
+// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+// import { useState } from 'react';
+// import { CircularProgressbar } from 'react-circular-progressbar';
+// import 'react-circular-progressbar/dist/styles.css';
+// import { useNavigate } from 'react-router-dom';
+
+// export default function CreatePost() {
+//   const [file, setFile] = useState(null);
+//   const [imageUploadProgress, setImageUploadProgress] = useState(null);
+//   const [imageUploadError, setImageUploadError] = useState(null);
+//   const [formData, setFormData] = useState({});
+//   const [publishError, setPublishError] = useState(null);
+// //   const navigate = useNavigate();
+
+//   const handleUploadImage = async () => {
+//     try {
+//       if (!file) {
+//         setImageUploadError('Please select an image');
+//         return;
+//       }
+
+//       setImageUploadError(null);
+//       setImageUploadProgress(0);
+
+//       const data = new FormData();
+//       data.append('file', file);
+//       data.append('upload_preset', "react_profile_upload");
+//       data.append('cloud_name', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
+
+//       const res = await fetch(
+//         `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
+//         {
+//           method: 'POST',
+//           body: data,
+//         }
+//       );
+
+//       const uploaded = await res.json();
+
+//       if (!uploaded.secure_url) {
+//         throw new Error('Upload failed');
+//       }
+
+//       setFormData({ ...formData, image: uploaded.secure_url });
+//       setImageUploadProgress(null);
+//     } catch (error) {
+//       console.error(error);
+//       setImageUploadError('Image upload failed');
+//       setImageUploadProgress(null);
+//     }
+//   };
+
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const res = await fetch('/api/post/create', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });
+//       const data = await res.json();
+//       if (!res.ok) {
+//         setPublishError(data.message);
+//         return;
+//       }
+//     //   navigate(`/post/${data.slug}`);
+ 
+//     } catch (error) {
+//       setPublishError('Something went wrong');
+//     }
+//   };
+
+//   return (
+//     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+//       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
+//       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+//         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+//           <TextInput
+//             type='text'
+//             placeholder='Title'
+//             required
+//             id='title'
+//             className='flex-1'
+//             onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+//           />
+//           <Select
+//             onChange={(e) =>
+//               setFormData({ ...formData, category: e.target.value })
+//             }
+//           >
+//             <option value='uncategorized'>Select a category</option>
+//             <option value='javascript'>JavaScript</option>
+//             <option value='reactjs'>React.js</option>
+//             <option value='nextjs'>Next.js</option>
+//           </Select>
+//         </div>
+
+//         {/* Image Upload Section */}
+//         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+//           <FileInput
+//             type='file'
+//             accept='image/*'
+//             onChange={(e) => setFile(e.target.files[0])}
+//           />
+//           <Button
+//             type='button' className="bg-gradient-to-br from-purple-600 to-blue-500 text-white"
+//             size='sm'
+//             outline
+//             onClick={handleUploadImage}
+//             disabled={!!imageUploadProgress}
+//           >
+//             {imageUploadProgress ? (
+//               <div className='w-16 h-16'>
+//                 <CircularProgressbar
+//                   value={imageUploadProgress}
+//                   text={`${imageUploadProgress || 0}%`}
+//                 />
+//               </div>
+//             ) : (
+//               'Upload Image'
+//             )}
+//           </Button>
+//         </div>
+
+//         {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+//         {formData.image && (
+//           <img
+//             src={formData.image}
+//             alt='upload'
+//             className='w-full h-72 object-cover'
+//           />
+//         )}
+
+//          <Editor
+  
+//   toolbarClassName="toolbarClassName"
+//   wrapperClassName="wrapperClassName"
+//   editorClassName="editorClassName min-h-[200px] p-2 focus:outline-none dark:bg-white dark:text-black font-size:1.05erm" onChange={(value) =>{
+//     setFormData({...formData, content: value})
+//   }}
+  
+// />;
+
+//         <Button type='submit' className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+//           Publish
+//         </Button>
+
+//         {publishError && (
+//           <Alert className='mt-5' color='failure'>
+//             {publishError}
+//           </Alert>
+//         )}
+//       </form>
+//     </div>
+//   );
+// }
+import { useState } from "react";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import { EditorState, convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { useState } from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate } from 'react-router-dom';
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
   const [file, setFile] = useState(null);
@@ -12,27 +175,32 @@ export default function CreatePost() {
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
   const [publishError, setPublishError] = useState(null);
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
   const navigate = useNavigate();
 
+  // 🔹 Upload image to Cloudinary
   const handleUploadImage = async () => {
     try {
       if (!file) {
-        setImageUploadError('Please select an image');
+        setImageUploadError("Please select an image");
         return;
       }
 
       setImageUploadError(null);
-      setImageUploadProgress(0);
+      setImageUploadProgress(10);
 
       const data = new FormData();
-      data.append('file', file);
-      data.append('upload_preset', "react_profile_upload");
-      data.append('cloud_name', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
+      data.append("file", file);
+      data.append("upload_preset", "react_profile_upload"); // your Cloudinary preset
+      data.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
 
       const res = await fetch(
-        `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`,
         {
-          method: 'POST',
+          method: "POST",
           body: data,
         }
       );
@@ -40,114 +208,137 @@ export default function CreatePost() {
       const uploaded = await res.json();
 
       if (!uploaded.secure_url) {
-        throw new Error('Upload failed');
+        throw new Error("Upload failed");
       }
 
-      setFormData({ ...formData, image: uploaded.secure_url });
+      setFormData((prev) => ({ ...prev, image: uploaded.secure_url }));
       setImageUploadProgress(null);
     } catch (error) {
       console.error(error);
-      setImageUploadError('Image upload failed');
+      setImageUploadError("Image upload failed");
       setImageUploadProgress(null);
     }
   };
 
+  // 🔹 Submit post data to backend
   const handleSubmit = async (e) => {
-    // e.preventDefault();
-    // try {
-    //   const res = await fetch('/api/post/create', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(formData),
-    //   });
-    //   const data = await res.json();
-    //   if (!res.ok) {
-    //     setPublishError(data.message);
-    //     return;
-    //   }
-    //   navigate(`/post/${data.slug}`);
-    // } catch (error) {
-    //   setPublishError('Something went wrong');
-    // }
+    e.preventDefault();
+
+    // Convert EditorState → HTML string
+    const rawContent = convertToRaw(editorState.getCurrentContent());
+    const htmlContent = draftToHtml(rawContent);
+
+    const postData = { ...formData, content: htmlContent };
+
+    try {
+      const res = await fetch("/api/post/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      setPublishError(null);
+      navigate(`/post/${data.slug}`);
+    } catch (error) {
+      console.error(error);
+      setPublishError("Something went wrong");
+    }
   };
 
   return (
-    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        {/* 🔹 Title and Category */}
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
-            type='text'
-            placeholder='Title'
+            type="text"
+            placeholder="Title"
             required
-            id='title'
-            className='flex-1'
-            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+            id="title"
+            className="flex-1"
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
           <Select
             onChange={(e) =>
               setFormData({ ...formData, category: e.target.value })
             }
           >
-            <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value="uncategorized">Select a category</option>
+            <option value="javascript">JavaScript</option>
+            <option value="reactjs">React.js</option>
+            <option value="nextjs">Next.js</option>
           </Select>
         </div>
 
-        {/* Image Upload Section */}
-        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+        {/* 🔹 Image Upload */}
+        <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
           <FileInput
-            type='file'
-            accept='image/*'
+            type="file"
+            accept="image/*"
             onChange={(e) => setFile(e.target.files[0])}
           />
           <Button
-            type='button' className="bg-gradient-to-br from-purple-600 to-blue-500 text-white"
-            size='sm'
-            outline
+            type="button"
+            className="bg-gradient-to-br from-purple-600 to-blue-500 text-white"
+            size="sm"
             onClick={handleUploadImage}
             disabled={!!imageUploadProgress}
           >
             {imageUploadProgress ? (
-              <div className='w-16 h-16'>
+              <div className="w-16 h-16">
                 <CircularProgressbar
                   value={imageUploadProgress}
                   text={`${imageUploadProgress || 0}%`}
                 />
               </div>
             ) : (
-              'Upload Image'
+              "Upload Image"
             )}
           </Button>
         </div>
 
-        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {/* 🔹 Error and Preview */}
+        {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
         {formData.image && (
           <img
             src={formData.image}
-            alt='upload'
-            className='w-full h-72 object-cover'
+            alt="upload"
+            className="w-full h-72 object-cover"
           />
         )}
 
-         <Editor
-  
-  toolbarClassName="toolbarClassName"
-  wrapperClassName="wrapperClassName"
-  editorClassName="editorClassName min-h-[200px] p-2 focus:outline-none dark:bg-white dark:text-black font-size:1.05erm"
-  
-/>;
+        {/* 🔹 Draft.js Editor */}
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={(state) => setEditorState(state)}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName min-h-[200px] p-2 focus:outline-none dark:bg-white dark:text-black text-[1.05rem]"
+        />
 
-        <Button type='submit' className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+        {/* 🔹 Publish Button */}
+        <Button
+          type="submit"
+          className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
           Publish
         </Button>
 
         {publishError && (
-          <Alert className='mt-5' color='failure'>
+          <Alert className="mt-5" color="failure">
             {publishError}
           </Alert>
         )}
