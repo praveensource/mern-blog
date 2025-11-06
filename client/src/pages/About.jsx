4da3543617d64c6bb4f3b6596bedcefc991f8b4c
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-11-06 11:49:25 +0530

    about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      about
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Blog App</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quaerat, veniam at corrupti dignissimos dicta officiis pariatur. Reiciendis fuga iure rem, quam harum corporis maiores asperiores, accusamus nulla mollitia officiis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum dolore obcaecati quos dignissimos molestias ratione, earum corrupti labore hic optio.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem laborum ratione vero cupiditate ullam? Ut possimus amet, eos blanditiis asperiores animi fuga ducimus consequuntur atque nemo maiores ipsum, deleniti nulla rem sapiente laudantium, adipisci laboriosam aspernatur ab. Odio, maiores!</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction';
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis incidunt recusandae saepe provident totam quod eligendi suscipit iusto autem exercitationem.</p>
+      <CallToAction />
     </div>
   )
 }
